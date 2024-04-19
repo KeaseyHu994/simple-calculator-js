4a3bfefa5793d4f001a6482ff130f9162080f364
PlzDonateRobuxCuzImPoor <153300591+KeaseyHu994@users.noreply.github.com>
2024-04-19 10:52:16 -0400

    full project

--- main.js
+++ main.js
@@ -3,9 +3,18 @@
 //JS Simple Calculator
 
 //input
-const num1 = prompt("Type in a number.");
-const num2 = prompt("Type in another number.");
+const num1 = parseInt(prompt("Type in a number."));
+const num2 = parseInt(prompt("Type in another number."));
 
 //process
+const add = num1 + num2;
+const sub = num1 - num2;
+const mult = num1 * num2;
+const div = num1 / num2;
 
 //output
+
+document.getElementById("ans1").innerHTML = `${num1} + ${num2} = ${add}`;
+document.getElementById("ans2").innerHTML = `${num1} - ${num2} = ${sub}`;
+document.getElementById("ans3").innerHTML = `${num1} * ${num2} = ${mult}`;
+document.getElementById("ans4").innerHTML = `${num1} / ${num2} = ${div}`;
